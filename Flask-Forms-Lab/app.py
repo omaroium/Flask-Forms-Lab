da4e98c6e0603fb3a3f6da638fe0f78df130902e
omaroium <omaralimycan@gmail.com>
2022-07-27 12:30:57 +0300

    with bonus suiii

--- Flask-Forms-Lab/app.py
+++ Flask-Forms-Lab/app.py
@@ -8,15 +8,46 @@ app = Flask(  # Create a flask app
 )
 
 
+accounts={
+	"llo2ay" : "123",
+	"bob" : "456"
+
+
+}
 username = "llo2ay"
-password = "123"
+password1 = "123"
 facebook_friends=["Loai","Yonathan","Adan", "George", "Fouad", "Celina"]
 
 
-@app.route('/')  # '/' for the default page
+@app.route('/' ,methods=['GET','POST'])  # '/' for the default page
 def login():
-  return render_template('login.html')
-  
+	facebook_friends=["Loai","Yonathan","Adan", "George", "Fouad", "Celina"]
+	if request.method=='GET':
+		return render_template('login.html')
+	else:
+		name = request.form['username']
+		password = request.form['password']
+		for x in (accounts):
+			if name==x and password==accounts[x]:
+				return render_template('home.html',name=name,password=password,facebook_friends=facebook_friends)
+		
+		return render_template('login.html')
+
+@app.route('/home' ,methods=['GET','POST'])  # '/' for the default page
+def home():
+
+  return render_template('home.html',facebook_friends=facebook_friends)
+@app.route('/friends_exist/<string:name>' ,methods=['GET','POST'])  # '/' for the default page
+def friends_exist(name):
+	y=0
+	exists= False
+	for x in (facebook_friends):
+		if name==x:
+			y+=1
+	if y>=1:
+		exists=True
+	return render_template('friend_exists.html',facebook_friends=facebook_friends,exists=exists,name1=name)
+		
 
 
 
